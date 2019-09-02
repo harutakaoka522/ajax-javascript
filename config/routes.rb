f8dc47ff80361a38e7488202244fee14c055f954
hogehoge <AAAgmail.com>
2019-09-02 09:41:54 +0900

    conversations（会話）設定の生成

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   resources :blogs do
     resources :comments
   end
+  resources :conversations do
+    resources :messages
+  end
   resources :users
   resources :relationships, only: [:create, :destroy]
 end
